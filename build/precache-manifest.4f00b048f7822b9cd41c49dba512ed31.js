074ca2ddcee4539fe1c0aeb45820f7f292b56f40
RickGove <63856101+RickGove@users.noreply.github.com>
2020-05-16 15:53:22 +0300

    Deployment Of Personal Site

---
+++ build/precache-manifest.4f00b048f7822b9cd41c49dba512ed31.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55a8c0a88899afa52a7e63be7d66cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "238abd954fcd76f62035",
+    "url": "/static/js/2.e501749f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e501749f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0256c72ca3d13590074",
+    "url": "/static/js/main.e64120b7.chunk.js"
+  },
+  {
+    "revision": "bd3bbfe48c82e953e6e6",
+    "url": "/static/js/runtime-main.b501d508.js"
+  },
+  {
+    "revision": "3745d5de0728ec47786eb328c7076356",
+    "url": "/static/media/logo_SC.3745d5de.png"
+  },
+  {
+    "revision": "deb4313555a8105113749d427b543e40",
+    "url": "/static/media/logo_construction.deb43135.png"
+  },
+  {
+    "revision": "f1f330057af169d9e725dae149d2ba75",
+    "url": "/static/media/logo_css.f1f33005.png"
+  },
+  {
+    "revision": "ca1bf078f29bbef7cb6c94770a917b73",
+    "url": "/static/media/logo_email.ca1bf078.png"
+  },
+  {
+    "revision": "8ba1a66e461f2659c489cc912dcba64f",
+    "url": "/static/media/logo_html.8ba1a66e.png"
+  },
+  {
+    "revision": "a146e3d909b29fa92e39fbfd3a496c9b",
+    "url": "/static/media/logo_linked.a146e3d9.png"
+  },
+  {
+    "revision": "e69f859b1607e7fd4ddc539722911d09",
+    "url": "/static/media/logo_npm.e69f859b.png"
+  },
+  {
+    "revision": "90e377a0adefbf009fa135b7519241e1",
+    "url": "/static/media/logo_react.90e377a0.png"
+  },
+  {
+    "revision": "443df9a00bb614cfaf82cc43a3fd9b69",
+    "url": "/static/media/logo_redux.443df9a0.png"
+  },
+  {
+    "revision": "9c3104cf237c1e01d71b65796882baf6",
+    "url": "/static/media/logo_twitter.9c3104cf.png"
+  },
+  {
+    "revision": "c9fccedde88ae25dce781ea7ed08c9f2",
+    "url": "/static/media/scrollTop.c9fccedd.png"
+  }
+]);
