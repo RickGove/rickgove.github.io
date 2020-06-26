38d8457cb2fd6229ee418ab8cf3ff87ad8310c6f
RickGove <63856101+RickGove@users.noreply.github.com>
2020-06-26 12:58:08 +0300

    try again with current build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c8b77e68ea2a38bce29c48185019fd1.js"
+  "/precache-manifest.979d5910123b47130d6cc94a50a87ea1.js"
 );
 
 self.addEventListener('message', (event) => {
