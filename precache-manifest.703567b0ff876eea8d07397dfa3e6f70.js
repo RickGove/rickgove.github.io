0e2c8f35e7b7d02b05be9cbdbb2ebe529fc8a06f
RickGove <63856101+RickGove@users.noreply.github.com>
2020-08-10 20:31:28 +0300

    single page to root out the gsap issues

---
+++ precache-manifest.703567b0ff876eea8d07397dfa3e6f70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6475f52f573126dbaa0de11dc71f1ec",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9c9910f763034da496e6",
+    "url": "/react-gh-pages/static/js/2.f7937763.chunk.js"
+  },
+  {
+    "revision": "81fc4a5d5bd295e7dda24772a5f5a9ff",
+    "url": "/react-gh-pages/static/js/2.f7937763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d14cc503a459d58b629",
+    "url": "/react-gh-pages/static/js/main.634d625a.chunk.js"
+  },
+  {
+    "revision": "e884568b8dff2c8a2090",
+    "url": "/react-gh-pages/static/js/runtime-main.ef1d6286.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44073bac10821dd63b06eb043380c084.js"
+  "/react-gh-pages/precache-manifest.703567b0ff876eea8d07397dfa3e6f70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
