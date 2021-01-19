f41ac4bcb78b8ff911f690de3fecf538edc949fb
Rick Gove <rickbgove@gmail.com>
2021-01-19 09:22:17 +0300

    Updated FunctionA

--- functions/functionA.js
+++ functions/functionA.js
@@ -1,3 +1,16 @@
 const functionA = () => {
 	console.log('HI EVERYONE!!');
+	var rng_nums = new Uint32Array(2);
+	if (window.crypto && window.crypto.getRandomValues)
+		window.crypto.getRandomValues(rng_nums);
+	// Chrome, Opera, Firefox
+	else if (window.msCrypto && window.msCrypto.getRandomValues)
+		window.msCrypto.getRandomValues(rng_nums);
+	// IE
+	else
+		rng_nums = [
+			Math.floor(Math.random() * 4294967295),
+			Math.floor(Math.random() * 4294967295),
+		]; // All outdated browsers
+	return (rng_nums[0] >>> 5) * 67108864.0 + (rng_nums[1] >>> 6);
 };
